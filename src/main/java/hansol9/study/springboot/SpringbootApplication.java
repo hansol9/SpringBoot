10027749cb8c4384b38dc7e845d4cdf9286df060
hansol9 <hansol9@gmail.com>
2020-03-28 14:28:07 -0400

    AutoConfiguration Practice#1

--- src/main/java/hansol9/study/springboot/SpringbootApplication.java
+++ src/main/java/hansol9/study/springboot/SpringbootApplication.java
@@ -1,14 +1,25 @@
 package hansol9.study.springboot;
 
+import me.whiteship.Holoman;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SpringbootApplication {
 
     public static void main(String[] args) {
 
-        SpringApplication.run(SpringbootApplication.class, args);
+        SpringApplication application = new SpringApplication(SpringbootApplication.class);
+        application.run(args);
+    }
+
+    @Bean
+    public Holoman holoman() {
+        Holoman holoman = new Holoman();
+        holoman.setName("Hello Holoman!!!");
+        holoman.setHowLong(60);
+        return holoman;
     }
 
 }
--- src/test/java/hansol9/study/springboot/SpringbootApplicationTests.java
+++ src/test/java/hansol9/study/springboot/SpringbootApplicationTests.java
@@ -1,5 +1,7 @@
 package hansol9.study.springboot;
 
+//import org.junit.jupiter.api.Test;
+//import org.junit.Test;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
