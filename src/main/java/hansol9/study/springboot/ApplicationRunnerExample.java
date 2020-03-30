ff977a160ebfe8a25c377bb146179e20b827a6c3
hansol9 <hansol9@gmail.com>
2020-03-30 10:38:00 -0400

    Adding Properties

--- src/main/java/hansol9/study/springboot/ApplicationRunnerExample.java
+++ src/main/java/hansol9/study/springboot/ApplicationRunnerExample.java
@@ -1,5 +1,6 @@
 package hansol9.study.springboot;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Component;
@@ -7,10 +8,21 @@ import org.springframework.stereotype.Component;
 @Component
 public class ApplicationRunnerExample implements ApplicationRunner {
 
+    @Value("${holoman.name}")
+    private String name;
+
+    @Value("${holoman.age}")
+    private int age;
+
+    @Value("${holoman.fullName}")
+    private String fullName;
+
     @Override
     public void run(ApplicationArguments args) throws Exception {
         System.out.println("Application Runner!!!");
         System.out.println("foo: " + args.containsOption("foo"));
         System.out.println("bar: " + args.containsOption("bar"));
+        System.out.println("Properties: " + name);
+        System.out.println("age: " + age);
     }
 }
--- src/test/java/hansol9/study/springboot/SpringbootApplicationTests.java
+++ src/test/java/hansol9/study/springboot/SpringbootApplicationTests.java
@@ -3,13 +3,29 @@ package hansol9.study.springboot;
 //import org.junit.jupiter.api.Test;
 //import org.junit.Test;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.env.Environment;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit4.SpringRunner;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+@RunWith(SpringRunner.class)
+//@TestPropertySource(properties = "holoman.name=annotation properties")
+@TestPropertySource(locations = "classpath:/application_test.properties")
 @SpringBootTest
+//@SpringBootTest(properties = "holoman.name=annotation properties")
 class SpringbootApplicationTests {
+
+    @Autowired
+    Environment environment;
 
     @Test
     void contextLoads() {
+        assertThat(environment.getProperty("holoman.name"))
+        .isEqualTo("Test for properties");
     }
 
 }
