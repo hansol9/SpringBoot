9aa21439c6bd81601e99362f4bd2005a1de80da2
hansol9 <hansol9@gmail.com>
2020-04-02 18:58:12 -0400

    Test Utils with OutputCapture

--- src/test/java/hansol9/study/springboot/sample/SliceControllerTest.java
+++ src/test/java/hansol9/study/springboot/sample/SliceControllerTest.java
@@ -1,13 +1,19 @@
 package hansol9.study.springboot.sample;
 
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.test.rule.OutputCapture;
+import org.springframework.boot.test.system.OutputCaptureExtension;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.io.OutputStream;
+
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
@@ -16,6 +22,9 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @RunWith(SpringRunner.class)
 @WebMvcTest(SampleController.class)
 public class SliceControllerTest {
+
+    @Rule
+    public OutputCapture outputCapture = new OutputCapture();
 
     @MockBean
     SampleService mockSampleService;
@@ -30,5 +39,9 @@ public class SliceControllerTest {
         mockMvc.perform(get("/hello"))
                 .andDo(print())
                 .andExpect(content().string("hello success"));
+
+        assertThat(outputCapture.toString())
+                .contains("holoman")
+                .contains("skip");
     }
 }
